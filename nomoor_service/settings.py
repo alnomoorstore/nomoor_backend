63f7ab02665d1b5c2c784a21c6b2bfc07abfa3fd
alnomoorstore <ibraheemalmalikyt@gmail.com>
2025-06-21 14:54:22 +0300

    Fix barcode and print receipt URL and views

--- nomoor_service/settings.py
+++ nomoor_service/settings.py
@@ -114,7 +114,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# المسار الذي سيُجمع فيه كل ملفات static عند تشغيل collectstatic
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
