e478e732117db6efe3f6323240a7affa90ae3916
alnomoorstore <ibraheemalmalikyt@gmail.com>
2025-06-21 15:18:46 +0300

    Fix barcode and print receipt URL and views

--- nomoor_service/settings.py
+++ nomoor_service/settings.py
@@ -120,8 +120,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-# المسار الذي سيُجمع فيه كل ملفات static عند تشغيل collectstatic
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
